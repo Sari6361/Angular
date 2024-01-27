c07bc5dfc6920ceb082d6741ef76773757bdf6a2
sari avramovich <s0556766361@gmail.com>
2024-01-27 21:53:24 +0200

    27.01.24

--- src/app/observable-exm/observable-exm.component.ts
+++ src/app/observable-exm/observable-exm.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Observable, interval, map } from 'rxjs';
+import { Observable, interval, map, filter, from } from 'rxjs';
 import { Student } from '../Entities/student.model';
 import { STUDENTS } from '../student.service';
 
@@ -14,22 +14,18 @@ export class ObservableExmpleComponent implements OnInit {
     for (; i < STUDENTS.length; i++)
       o.next(STUDENTS[i].id + ": " + STUDENTS[i].name)
   })
+  studesnts_: any[] = [];
+  students2: Observable<string> = from(this.studesnts_).pipe(map(x => x.name))
 
-  students2: Observable<Student> = new Observable((o) => {
-    let i = 0;
-    for (; i < STUDENTS.length; i++)
-      o.next(/*STUDENTS[i]*/);
-  })
-  
   timer?: string;
 
-  timer1: Observable<Date> = new Observable((o) => {
+  timer1: Observable<Date> = new Observable((o => {
     setInterval(() => {
-      o.next(new Date);
+      o.next(new Date());
     }, 1000)
-  });
+  }));
 
-  timer2: Observable<Date>=interval(1000).pipe(map(timer=>new Date()));
+  timer2: Observable<Date> = interval(1000).pipe(map(timer => new Date()));
   @Input()
   name: String;
 
@@ -41,10 +37,44 @@ export class ObservableExmpleComponent implements OnInit {
     }
   })
 
+  send1: Observable<Student> = new Observable(x => {
+    let i = 0;
+    for (; i < STUDENTS.length; i++)
+      if (STUDENTS[i].active)
+        x.next(STUDENTS[i]);
+  })
+
+  sendEmail1() {
+    this.send1.subscribe(v => {
+      alert("email successfully sent to " + v.name);
+    })
+  }
+  
+  send_students: any[] = [];
+  send2: Observable<Student> = from(this.send_students);
+  sendEmail2() {
+    this.send2.pipe(filter(x => x.active),
+      map(x => "email successfully sent to " + x.name)).subscribe(x => {
+        alert(x);
+      })
+  }
+
+
   constructor() {
+
+    this.students.subscribe(x => {
+      console.log('next:', x)
+    });
+
+    // this.students2.pipe(map(x => x.name)).subscribe(x => console.log("s", x));
+
+
     this.source.subscribe((val) => {
       this.students_names.push(val);
     });
+
+    this.timer2.subscribe(x =>
+      this.timer = x.toLocaleTimeString());
   }
   ngOnInit(): void {
   }
